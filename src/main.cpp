c56daac0b807805062950509f041b819240f30ca
Ethan Ostroff <ethanostroff1@gmail.com>
2026-04-07 10:05:44 -0400

    Refactor main.cpp to initialize handlers and remove unused function

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
 #include <Arduino.h>
+#include "bin_manager.h"
+#include "espnow_handler.h"
+#include "web_server.h"
 
-// put function declarations here:
-int myFunction(int, int);
+BinManager binManager;
+ESPNowHandler espNowHandler;
+WebHandler webHandler;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  binManager.begin();
+  espNowHandler.begin(&binManager);
+  webHandler.begin(&binManager);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  webHandler.loop();
 }
